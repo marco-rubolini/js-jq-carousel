d1ef5e2dd5667d7bf354309fe159da6551f6204e
Marco <rubolini.marco@gmail.com>
2020-05-04 17:14:48 +0200

    Ottimizzo il codice
    Ho creato delle funzioni da richiamare per far scorrere le immagini, farle avanzare e farle tornare indietro.

--- main.js
+++ main.js
@@ -1,6 +1,59 @@
-// Imposto l'azione con il clik su pulsante prev
+// Dichiaro una variabile con all'interno il metodo setInterval per far partire
+// automaticamente lo scorrimento delle immagini ogni 3 secondi
+
+var clock = setInterval(nextImage, 3000);
+
+// Imposto l'azione con il clik su pulsante next
 $('.next').click(function() {
-    // Recupero l'immagine che ha la clase active ora
+    // richiamo la funzione nextImage
+    nextImage();
+})
+
+// Imposto l'azione con il clik su pulsante prev
+$('.prev').click(function() {
+    // richiamo la funzione prevImage
+    prevImage();
+})
+
+// intercetto il click sul pallino
+$('.fa-circle').click(function() {
+    // Imposto una variabile che rappresenta l'indice delle immagini che parte da 0
+    var current_img = 0;
+    // rimuovo dal pallino attivo con indice 0 la classe active
+    $('.fa-circle.active').removeClass('active');
+    //aggiungo al pallino cliccato la classe active
+    $(this).addClass("active");
+    // rimuovo dall'immagine con indice 0 la classe attiva
+    $('img.active').removeClass('active');
+    // imposto il valore nuovo dell'indice delle immagini prendendo i dati dal data-num dell'html
+    current_img=$(this).data('num');
+    //aggiungo la classe active all'immagine nuova corrente
+	$('img').eq(current_img).addClass('active');
+});
+
+// intercetto il clik sul pulsante pausa
+$('.pause').click(function(){
+    // interrompo la funzione setInterval
+    clearInterval(clock);
+    // mostro il pulsante play
+    $('.controller .play').show();
+    // nascondo il pulsante pausa
+    $('.controller .pause').hide();
+});
+
+// intercetto il clik sul pulsante play
+$('.play').click(function(){
+    // nascondo il pulsante play
+    $('.controller .play').hide();
+    // visualizzo il pulsante pause
+    $('.controller .pause').show();
+    // imposto la funzione setInterval al click del pulsante play
+    clock = setInterval(nextImage, 3000);
+});
+
+// Funzione nexImage
+function nextImage(){
+    // Recupero l'immagine attiva
     var currentImage = $('img.active');
     // Recupero il pallino corrente
     var currentCircle = $('.fa-circle.active');
@@ -31,12 +84,11 @@ $('.next').click(function() {
         // non c'è un pallino successivo
         // riparto dal primo
         $('.fa-circle:first-child').addClass('active')
-    }
+};
+};
 
-})
-
-// Imposto l'azione con il clik su pulsante prev
-$('.prev').click(function() {
+// Funzione prevImage
+function prevImage(){
     // Recupero l'immagine che ha la clase active ora
     var currentImage = $('img.active');
 
@@ -68,105 +120,4 @@ $('.prev').click(function() {
         // riparto dall'ultimo
         $('.fa-circle:last-child').addClass('active')
     }
-
-})
-// intercetto il click sul pallino
-$('.fa-circle').click(function() {
-    // Imposto una variabile che rappresenta l'indice delle immagini che parte da 0
-    var current_img = 0;
-    // rimuovo dal pallino attivo con indice 0 la classe active
-    $('.fa-circle.active').eq(current_img).removeClass('active');
-    //aggiungo al pallino cliccato la classe active
-    $(this).addClass("active");
-    // rimuovo dall'immagine con indice 0 la classe attiva
-    $('img.active').eq(current_img).removeClass('active');
-    // imposto il valore nuovo dell'indice delle immagini prendendo i dati dal data-num dell'html
-    current_img=+$(this).data('num');
-    //aggiungo la classe active all'immagine nuova corrente
-	$('img').eq(current_img).addClass('active');
-});
-
-var clock = setInterval(function() {
-    var currentImage = $('img.active');
-    // Recupero il pallino corrente
-    var currentCircle = $('.fa-circle.active');
-
-    // rimuovo la classe active dall'immagine corrente
-    currentImage.removeClass('active');
-
-    // rimuovo la classe active dal pallino corrente
-    currentCircle.removeClass('active');
-    // Recupero l'immagine successiva
-    var nextImage = currentImage.next('img');
-
-    // Recupero il pallino successivo
-    var nextCircle = currentCircle.next('.fa-circle');
-    // Verifico se esiste un'immagine successiva
-    if (nextImage.length != 0) {
-        // è presente un'immagine successiva
-        // aggiungo la classe active all'immagine successiva
-        nextImage.addClass('active');
-        // aggiungo la classe active al pallino successivo
-        nextCircle.addClass('active');
-    } else {
-        // non c'è un'immagine successiva
-        // Riparto dalla prima immagine
-        nextImage = $('img:first-child');
-        nextImage.addClass('active');
-
-        // non c'è un pallino successivo
-        // riparto dal primo
-        $('.fa-circle:first-child').addClass('active')
-    }
-}, 3000);
-// intercetto il clik sul pulsante pausa
-$('.pause').click(function(){
-    // interrompo la funzione setInterval
-    clearInterval(clock);
-    // mostro il pulsante play
-    $('.controller .play').show();
-    // nascondo il pulsante pausa
-    $('.controller .pause').hide();
-});
-
-// intercetto il clik sul pulsante play
-$('.play').click(function(){
-    // nascondo il pulsante play
-    $('.controller .play').hide();
-    // visualizzo il pulsante pause
-    $('.controller .pause').show();
-    // imposto la funzione setInterval al click del pulsante play
-    clock = setInterval(function() {
-        var currentImage = $('img.active');
-        // Recupero il pallino corrente
-        var currentCircle = $('.fa-circle.active');
-
-        // rimuovo la classe active dall'immagine corrente
-        currentImage.removeClass('active');
-
-        // rimuovo la classe active dal pallino corrente
-        currentCircle.removeClass('active');
-        // Recupero l'immagine successiva
-        var nextImage = currentImage.next('img');
-
-        // Recupero il pallino successivo
-        var nextCircle = currentCircle.next('.fa-circle');
-        // Verifico se esiste un'immagine successiva
-        if (nextImage.length != 0) {
-            // è presente un'immagine successiva
-            // aggiungo la classe active all'immagine successiva
-            nextImage.addClass('active');
-            // aggiungo la classe active al pallino successivo
-            nextCircle.addClass('active');
-        } else {
-            // non c'è un'immagine successiva
-            // Riparto dalla prima immagine
-            nextImage = $('img:first-child');
-            nextImage.addClass('active');
-
-            // non c'è un pallino successivo
-            // riparto dal primo
-            $('.fa-circle:first-child').addClass('active')
-        }
-    }, 3000);
-});
+}
